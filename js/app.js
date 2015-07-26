aec72b875dfae282e82c553d1ce87b8c3257cbec
shui91 <sherman.sy.hui@gmail.com>
2015-07-26 00:16:36 -0700

    feat: added EventListener for Player control

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var SPACEBG = 'images/space-bg.jpg';
 // set up basic gameObj as superclass
 var gameObj = function(){
 	this.sprite ='';
-}
+};
 
 gameObj.prototype.render = function(){
 	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -23,5 +23,30 @@ var Player = function() {
 
 Player.prototype = Object.create(gameObj.prototype);
 Player.prototype.constructor = Player;
+// For every downKey, the Player will move accordingly
+Player.prototype.update = function(dt){
+	if (this.downKey === 'left'){
+
+	}else if(this.downKey === 'right'){
+
+	}else if(this.downKey === 'up'){
+
+	}else if(this.downKey === 'down'){
+
+	}
+	this.downKey = null;
+};
 
 var player = new Player();
+
+// downKey Listener provided by Udacity
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
